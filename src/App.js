019b39a07f23080d66bade6fa85df4a9a1b7fd92
obusorezekiel <obusorezekiel@gmail.com>
2020-01-04 05:22:16 +0100

    WORKING WELL

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there
+          Hello there
         </p>
         <a
           className="App-link"
